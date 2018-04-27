a5b0469cefa9c567bdfb40caabb2ad81141799c9
Tianlang Gao <gtl@berkeley.edu>
2018-04-26 20:22:37 -0700

    Merged all WB, IMF, OECD data into econ_outcomes.csv

--- merge.R
+++ merge.R
@@ -18,3 +18,19 @@ m$Country.name[is.na(m$Country.name)] <- m$country.name[is.na(m$Country.name)]
 colnames(m)[4] <- "cown"
 m$cown[is.na(m$cown)] <- m$cown.y[is.na(m$cown)]
 write.csv(m, "imf_oecd.csv")
+
+# Merge WB and IMF&OECD data
+wb <- read.csv("worldbank.csv")
+imf_oecd <- read.csv("imf_oecd.csv")
+m <- merge(wb, imf_oecd, by=c("iso","year"), all=TRUE, sort = TRUE)
+m$cown.x[is.na(m$cown.x)] <- m$cown.y[is.na(m$cown.x)]
+write.csv(m, "econ_outcomes.csv")
+
+# Modify incorrect terms in econ_outcomes
+econ <- read.csv("econ_outcomes.csv")
+library(countrycode)
+econ$cown <- countrycode(econ$iso, "iso3c", "cown", warn = TRUE)
+region <- read.csv("countryclass.csv")
+econ <- merge(econ, region, by=c("iso"), all=TRUE, sort = TRUE)
+econ$region.x <- NULL
+write.csv(econ, "econ.csv")
